af5d9b8f1bd2f4084a684b6e1d2ee6463b45aff3
John Crepezzi <john.crepezzi@gmail.com>
2013-11-27 16:33:41 -0500

    More work

--- PanHandle/luhn_check.cl
+++ PanHandle/luhn_check.cl
@@ -1,15 +1,14 @@
-// TODO add note here
-kernel void luhn_check(global unsigned long* candidates) {
-
-    size_t i = get_global_id(0);
-    unsigned long num = candidates[i];
+__kernel void luhn_check(__global unsigned long *candidates) {
+    
+    size_t gid = get_global_id(0);
+    unsigned long num = candidates[gid];
     
     short digit;
     short sum = 0;
-    short dbl = 0; // false
+    short dbl = 0; // true, starting at right
     
     while (num > 0) {
-
+        
         digit = num % 10;
         
         if (dbl) {
@@ -20,13 +19,13 @@ kernel void luhn_check(global unsigned long* candidates) {
         }
         
         sum += digit;
-        dbl ^= dbl;
+        dbl = !dbl;
         num /= 10;
         
     }
-
+    
     if (sum % 10 != 0) {
-        candidates[i] = 0; // TODO add note here
+        candidates[gid] = 0;
     }
-
+    
 }
--- PanHandle/main.c
+++ PanHandle/main.c
@@ -14,24 +14,21 @@
 #include "luhn_check.cl.h"
 #include "msha_kernel.cl.h"
 
-#define NUM_VALUES 1024
-#define KEY_LENGTH 16
-
-// Our main function
-int main(int argc, const char * argv[]) {
-
+void doit(unsigned long start, unsigned long num_values) {
+    
     // read in all lines
-    char lines[1022][28];
+    char lines[1022][29];
     FILE *fp = fopen("/Users/john/Development/PanHandle/data/hashes.txt", "r");
     for (int i = 0; i < 1022; i++) { // TODO flex
-        fgets(lines[i], 28, fp);
+        fgets(lines[i], 29, fp);
     }
     fclose(fp);
     
     // decode each line
-    char choices[1022][20];
+    // TODO note for why i did this here
+    cl_uchar choices[1022][20];
     for (int i = 0; i < 1022; i++) { // TODO flex
-        Base64decode(choices[i], lines[i]);
+        Base64decode((void *)choices[i], lines[i]);
     }
         
     // Create out dispatch queue, prefer GPU but allow fallback
@@ -39,27 +36,32 @@ int main(int argc, const char * argv[]) {
     if (queue == NULL) {
         queue = gcl_create_dispatch_queue(CL_DEVICE_TYPE_CPU, NULL);
     }
+
+    dispatch_queue_t queue2 = gcl_create_dispatch_queue(CL_DEVICE_TYPE_CPU, NULL);
+    if (queue2 == NULL) {
+        queue2 = gcl_create_dispatch_queue(CL_DEVICE_TYPE_GPU, NULL);
+    }
     
     // Put together our test data
-    cl_ulong* candidates = (cl_ulong*)malloc(sizeof(cl_ulong) * NUM_VALUES);
-    for (int i = 0; i < NUM_VALUES; i++) {
-        candidates[i] = 5462916022532590L + i;
+    cl_ulong* candidates = (cl_ulong*)malloc(sizeof(cl_ulong) * num_values); // TODO reuse this array on subsequent runs
+    for (int i = 0; i < num_values; i++) {
+        candidates[i] = start + i;
     }
-    printf("starting with %d\n", NUM_VALUES);
+    printf("starting with %ld at %ld\n", num_values, start);
     
     // perform our luhn checks, setting anything to 0 that doesn't pass
-    void* cl_candidates = gcl_malloc(sizeof(cl_ulong) * NUM_VALUES, candidates, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
+    void* cl_candidates = gcl_malloc(sizeof(cl_ulong) * num_values, candidates, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR);
     dispatch_sync(queue, ^{
         size_t wgs;
         gcl_get_kernel_block_workgroup_info(luhn_check_kernel, CL_KERNEL_WORK_GROUP_SIZE, sizeof(wgs), &wgs, NULL);
-        cl_ndrange range = { 1, {0, 0, 0}, {NUM_VALUES, 0, 0}, {wgs, 0, 0}};
+        cl_ndrange range = {1, {0, 0, 0}, {num_values, 0, 0}, {wgs, 0, 0}};
         luhn_check_kernel(&range, (cl_ulong*)cl_candidates);
-        gcl_memcpy(candidates, cl_candidates, sizeof(cl_ulong) * NUM_VALUES);
+        gcl_memcpy(candidates, cl_candidates, sizeof(cl_ulong) * num_values);
     });
     
     // count the number of remaining numbers
     unsigned int count = 0;
-    for (int j = 0; j < NUM_VALUES; j++) {
+    for (int j = 0; j < num_values; j++) {
         if (candidates[j] != 0) {
             count += 1;
         }
@@ -69,50 +71,73 @@ int main(int argc, const char * argv[]) {
     // and convert the leftovers into a char array
     cl_uchar numbers[count * 16 * sizeof(cl_uchar)];
     void *ptr = numbers;
-    for (int j = 0; j < NUM_VALUES; j++) {
+    for (int j = 0; j < num_values; j++) {
         if (candidates[j] != 0) {
-            sprintf(ptr, "%lld", candidates[j]);
-            ptr += sizeof(unsigned char) * 16;
+            sprintf(ptr, "%lld ", candidates[j]);
+            ptr += sizeof(cl_uchar) * 16;
         }
     }
-    
+        
     // now we're ready to SHA1 the keys
+    short key_length = 5;
     void* cl_keys = gcl_malloc(sizeof(cl_uchar) * count * 16, numbers, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
-    void* cl_results = gcl_malloc(sizeof(cl_uint) * count * 5, NULL, CL_MEM_WRITE_ONLY);
-    void* hashes = malloc(sizeof(cl_uint) * count * 5);
-    dispatch_sync(queue, ^{
+    void* cl_results = gcl_malloc(sizeof(cl_uint) * count * key_length, NULL, CL_MEM_WRITE_ONLY);
+    void* hashes = malloc(sizeof(cl_uint) * count * key_length);
+    dispatch_sync(queue2, ^{
         size_t wgs;
         gcl_get_kernel_block_workgroup_info(sha1_crypt_kernel_kernel, CL_KERNEL_WORK_GROUP_SIZE, sizeof(wgs), &wgs, NULL);
-        cl_ndrange range = { 1, {0, 0, 0}, {count, 0, 0}, {wgs, 0, 0}};
+        cl_ndrange range = {1, {0, 0, 0}, {count, 0, 0}, {wgs, 0, 0}};
         sha1_crypt_kernel_kernel(&range, (cl_uint*)cl_keys, (cl_uint*)cl_results);
-        gcl_memcpy(hashes, cl_results, sizeof(cl_uint) * count * 5);
+        gcl_memcpy(hashes, cl_results, sizeof(cl_uint) * count * key_length);
     });
-    printf("produced SHA");
+    printf("produced SHA\n");
     
     // we've got outselves the keys, and now we can go through them looking for matches
-    // TODO use hashmap
+    // TODO use hashmap of some sort for comparison
     for (int i = 0; i < 1022; i++) {
         void *ptr = hashes;
         for (int j = 0; j < count; j++) {
-            if (memcmp(choices[i], ptr, 5) == 0) {
-                printf("found: %hhu!", numbers[j]);
+            
+            cl_uchar temp[16];
+            void *pre = numbers;
+            pre += j * sizeof(cl_uchar);
+            memcpy(temp, pre, 16 * sizeof(cl_uchar));
+            printf("number: %s\n", temp);
+            
+            cl_uchar sha[sizeof(cl_uint) * key_length];
+            pre = hashes;
+            pre += j * sizeof(cl_uint) * key_length;
+            memcpy(sha, pre, sizeof(cl_uint) * key_length);
+            printf("sha: %s\n", sha);
+            
+            printf("raw: %s\n", lines[i]);
+            printf("com: %s\n", choices[i]);
+            
+            if (memcmp(choices[i], ptr, sizeof(cl_uint) * key_length) == 0) {
+                printf("found: %hhu!\n", numbers[j]);
             }
+
         }
-        ptr += 5;
+        ptr += key_length * sizeof(cl_uint);
     }
-    
-    // Show our results to the world
-    //for (int j = 0; j < NUM_VALUES; j++) {
-    //    printf("%lld !!\n", candidates[j]);
-    //}
     
     // Clean up after ourselves
     gcl_free(cl_candidates); gcl_free(cl_keys); gcl_free(cl_results);
     free(candidates); free(hashes);
     dispatch_release(queue);
+    dispatch_release(queue2);
     
-    // And exit.
-    return 0;
-
 }
 
+int main(int argc, const char * argv[]) {
+    
+    long start = 4242424242424242;
+    long step = 1024;
+    for (int i = 0; i < 1; i++) {
+        doit(start, step);
+        start += step;
+    }
+    
+    return 0;
+    
+}
